0de0f18c66196328bb5ba7238b613d7d3bede20f
Santi <santimcs@hotmail.com>
2011-01-30 11:49:02 +0700

    Done with bank project app

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ActiveRecord::Base
+	belongs_to :project
+	validates_presence_of :sequence, :name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 BankProject::Application.routes.draw do
+  resources :tasks
+
+  resources :projects
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
