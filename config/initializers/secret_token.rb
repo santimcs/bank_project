36a9f67634d7775bce5eb337c7dcd42f376f55be
Santi <santimcs@hotmail.com>
2011-01-30 10:15:18 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BankProject::Application.config.secret_token = 'afe0cc2a067c95e461c8e1d90d517f60dc2ef5a387ea531996586bfc0d280db6a86958b784f237d12902b7dda87d909964a22df083aed9b4d2556b63335cb50a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BankProject::Application.config.session_store :cookie_store, :key => '_bank_project_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# BankProject::Application.config.session_store :active_record_store
